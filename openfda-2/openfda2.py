f330c407f3b1fc34b6c3cff35d8d04752aca3c66
pjdisi <pablojdiegosimon@gmail.com>
2018-03-23 16:48:22 +0100

    Upload practices

--- openfda-2/openfda2.py
+++ openfda-2/openfda2.py
@@ -4,7 +4,7 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.github.com")
-conn.request("GET", "/orgs/elastic/repos", None, headers)
+conn.request("GET", "/users/orgs/elastic/repos", None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
@@ -13,7 +13,6 @@ conn.close()
 repos = json.loads(repos_raw)
 
 print("The total number of repostories is ",len(repos))
-x=0
-while x<30:
-    print("The owner of the first repository is", repos[x]['full_name'])
-    x+=1
+
+for elements in repos["results"]:
+	print(elements["id"]) 
